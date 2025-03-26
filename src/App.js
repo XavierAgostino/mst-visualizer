eb56f3cbc9279490c5c3ba1dbee6863da3740dd2
Xavier Agostino <xavieragostino@college.harvard.edu>
2025-03-26 10:14:09 -0400

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MSTVisualizer from './MSTVisualizer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <MSTVisualizer />
+      <Analytics />
     </div>
   );
 }
